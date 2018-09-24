c4439495f3e802d8caa2edb298b652f0cacfbaf4
Kyle <43512442+chapmankyle@users.noreply.github.com>
2018-09-25 00:21:20 +0200

    added an integer-to-binary converter

---
+++ int2binary.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 9
+
+char *int2bin(int a, char *buffer, int buff_size);
+
+int main()
+{
+	int val = 0;
+	char buffer[BUF_SIZE];
+	buffer[BUF_SIZE - 1] = '\0';
+
+	val = 14;
+	int2bin(val, buffer, BUF_SIZE - 1);
+	printf("%d in binary is :\t%s\n", val, buffer);
+
+	return EXIT_SUCCESS;
+}
+
+/* @brief Converts integer to binary string
+ *
+ * @param[in]	a
+ * 		integer to be converted to binary
+ * @param[out]	buffer
+ * 		string representation of binary of integer
+ * @param[in]	buf_size
+ * 		size of buffer
+ * @return		binary string representation of integer
+ */
+char *int2bin(int a, char *buffer, int buf_size) {
+	int i;
+    buffer += (buf_size - 1);
+
+    for (i = buf_size - 1; i >= 0; i--) {
+        *buffer-- = (a & 1) + '0';
+
+		a >>= 1;
+    }
+
+    return buffer;
+}
