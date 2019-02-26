20e6e7624cef5e7e4d08149b7babe72d2f00cc11
Kyle Chapman <kyleichapman@gmail.com>
2019-02-26 14:57:38 +0200

    changed spacing

--- include/smath.h
+++ include/smath.h
@@ -1,6 +1,6 @@
 /*
- * A Simple Math library for any C/C++ program.
- *    for license: see LICENSE.txt
+ * @brief A Simple Math library for any C/C++ program.
+ *    for license: see LICENSE
  *
  * @author Kyle Chapman
  * @date 01-02-2019
@@ -19,152 +19,173 @@ extern "C" {
 #define S_PI    3.1415926535897932385
 
 /* define all inline functions */
-#define smax(a, b)      (a > b ? a : b)
-#define smin(a, b)      (a < b ? a : b)
-#define sabs(a)         (a >= 0 ? a : -a)
-#define ssquare(a)      (a * a)
+#define smax(a, b)		(a > b ? a : b)
+#define smin(a, b)		(a < b ? a : b)
+#define sabs(a)			(a >= 0 ? a : -a)
+#define ssquare(a)		(a * a)
 
-#define sfloor(a)       ((int) a)
-#define ceil_pos(a)     ((a - (int) a) > 0 ? (int) (a + 1) : (int) a)
-#define ceil_neg(a)     ((a - (int) a) < 0 ? (int) (a - 1) : (int) a)
-#define sceil(a)        (a > 0 ? ceil_pos(a) : ceil_neg(a))
+#define sfloor(a)		((int) a)
+#define ceil_pos(a)		((a - (int) a) > 0 ? (int) (a + 1) : (int) a)
+#define ceil_neg(a)		((a - (int) a) < 0 ? (int) (a - 1) : (int) a)
+#define sceil(a)		(a > 0 ? ceil_pos(a) : ceil_neg(a))
 
 /* type definitions for convenience */
-typedef signed int          int32_t;
-typedef unsigned int        uint32_t;
-typedef signed long int     int64_t;
-typedef unsigned long int   uint64_t;
+typedef signed int			int32_t;
+typedef unsigned int		uint32_t;
+typedef signed long int		int64_t;
+typedef unsigned long int	uint64_t;
 
-typedef enum { false = 0, true = 1 } boolean;
+typedef enum { 
+	false = 0, 
+	true = 1 
+} boolean;
 
 /*
- * Rounds [f] to the nearest 1.
- * @param   f
- *      float to round
- * @return  rounded float to nearest 1
+ * @brief Rounds [f] to the nearest 1.
+ * 
+ * @param f
+ * 		The float to round
+ * @return Rounded float to nearest 1
  */
 int32_t sround(float f)
 {
-    return (int32_t) (f + 0.5f);
+	return (int32_t) (f + 0.5f);
 }
 
 /*
- * Rounds [f] to nearest integer [nearest].
- * @param   f
- *      float to round
- * @param   nearest
- *      nearest number to round to
- * @return  [f] rounded to the nearest integer [nearest]
+ * @brief Rounds [f] to nearest integer [nearest].
+ * 
+ * @param f
+ * 		The float to round to nearest integer
+ * @param nearest
+ * 		The nearest number to round to
+ * @return f rounded to the nearest integer [nearest]
  */
 int32_t sround_nearest(float f, int32_t nearest)
 {
-    return sround(f / nearest) * nearest;
+	return sround(f / nearest) * nearest;
 }
 
 /*
- * Computes the power of function (base ^ expo).
- * @param   base
- *      base number
- * @param   expo
- *      exponent
+ * @brief Computes the power of function (base ^ expo).
+ *
+ * @param base
+ *      The base number
+ * @param expo
+ *      The exponent
  * @return  [base] to-the-power-of [expo]
  */
 float spow(float base, uint32_t expo)
 {
-    float result = 1.0;
+	float result = 1.0;
 
-    for (;;) {
-        if (expo & 1) { result *= base; }
-        expo >>= 1;
-        if (!expo) { break; }
-        base *= base;
-    }
+	/* infinite for loop */
+	for (;;) {
+		if (expo & 1) { 
+			result *= base; 
+		}
 
-    return result;
+		expo >>= 1;
+
+		if (!expo) { 
+			break; 
+		}
+
+		base *= base;
+	}
+
+	return result;
 }
 
 /*
- * Computes the power function for integers only.
- * @param   base
- *      base number
- * @param   expo
- *      exponent
- * @return  [base] to-the-power-of [expo]
+ * @brief Computes the power function for integers only.
+ *
+ * @param base
+ * 		The base number
+ * @param expo
+ * 		The exponent
+ * @return [base] to-the-power-of [expo]
  */
 uint64_t spowd(uint32_t base, uint32_t expo)
 {
-    return (uint32_t) (spow((float) base, expo));
+	return (uint32_t) (spow((float) base, expo));
 }
 
 /*
- * Computes the exponent function (S_E ^ expo).
- * @param   expo
- *      exponent
- * @return  [S_E] to-the-power-of [expo]
+ * @brief Computes the exponent function (S_E ^ expo).
+ *
+ * @param expo
+ * 		The exponent
+ * @return [S_E] to-the-power-of [expo]
  */
 float sexp(uint32_t expo)
 {
-    return spow(S_E, expo);
+	return spow(S_E, expo);
 }
 
 /*
- * Computes the square root using the fast, inverse square root method.
- * @param   f
- *      float to compute square root of
- * @return  square root of [f]
+ * @brief Computes the square root using the fast, inverse square root method.
+ *
+ * @param f
+ *      Float to compute square root of
+ * @return square root of [f]
  */
 float ssqrt(float f)
 {
-    long i;
-    float x, y;
-    const float threehalfs = 1.5;
-    x = f * 0.5;
-    y = f;
-    i = *(long *) &y;
-    i = 0x5f3759df - (i >> 1);
-    y = *(float *) &i;
-    y = y * (threehalfs - (x * y * y));
-    return 1.0 / y;
+	long i;
+	float x, y;
+	const float threehalfs = 1.5;
+	x = f * 0.5;
+	y = f;
+	i = *(long *) &y;
+	i = 0x5f3759df - (i >> 1);
+	y = *(float *) &i;
+	y = y * (threehalfs - (x * y * y));
+	return 1.0 / y;
 }
 
 /*
- * Computes the sine of angle [theta] using Taylor Series expansion
- * @param   theta
- *      angle to find sine of
- * @return  sine of angle [theta] in radians
+ * @brief Computes the sine of angle [theta] using Taylor Series expansion
+ *
+ * @param theta
+ * 		Angle to find sine of
+ * @return Approximation of sine of angle [theta] in radians
  */
 float ssin(float theta)
 {
-    return (theta - (spow(theta, 3) / 6) + (spow(theta, 5) / 120) -
-            (spow(theta, 7) / 5040));
+	return (theta - (spow(theta, 3) / 6) + (spow(theta, 5) / 120) -
+			(spow(theta, 7) / 5040));
 }
 
 /*
- * Computes the cosine of angle [theta] using Taylor Series expansion
- * @param   theta
- *      angle to find cosine of
- * @return  cosine of angle [theta] in radians
+ * @brief Computes the cosine of angle [theta] using Taylor Series expansion
+ *
+ * @param theta
+ * 		Angle to find cosine of
+ * @return Approximation of cosine of angle [theta] in radians
  */
 float scos(float theta)
 {
-    return (1 - (spow(theta, 2) / 2) + (spow(theta, 4) / 24) -
-            (spow(theta, 6) / 720));
+	return (1 - (spow(theta, 2) / 2) + (spow(theta, 4) / 24) -
+			(spow(theta, 6) / 720));
 }
 
 /*
- * Computes the arc-tan of angle [theta]
- * @param   theta
- *      angle to find arc-tan of
- * @return  arc-tan of angle [theta]
+ * @brief Computes the arc-tan of angle [theta] using Taylor Series expansion
+ *
+ * @param theta
+ * 		Angle to find arc-tan of
+ * @return Approximation of arc-tan of angle [theta]
  */
 float sarctan(float theta)
 {
-    return (theta - (spow(theta, 3) / 3) + (spow(theta, 5) / 5) -
-            (spow(theta, 7) / 7));
+	return (theta - (spow(theta, 3) / 3) + (spow(theta, 5) / 5) -
+			(spow(theta, 7) / 7));
 }
 
 /* define at bottom so it can use functions above */
-#define distance(ax, ay, bx, by)    ssqrt(ssquare((float)bx - (float)ax) + ssquare((float)by - (float)ay))
+#define distance(ax, ay, bx, by) ssqrt(ssquare((float)bx - (float)ax) + \
+			ssquare((float)by - (float)ay))
 
 #ifdef __cplusplus
 }
--- include/svector.h
+++ include/svector.h
@@ -1,6 +1,6 @@
 /*
- * A Simple 2D Vector library for any C/C++ program.
- *    for license: see LICENSE.txt
+ * @brief A Simple 2D Vector library for any C/C++ program.
+ *    for license: see LICENSE
  *
  * @author Kyle Chapman
  * @date 03-02-2019
@@ -19,12 +19,12 @@ extern "C" {
 
 /* define the 2D vector for integer values */
 typedef struct {
-    int x, y;
+	int x, y;
 } sVec2i;
 
 /* define the 2D vector for float values */
 typedef struct {
-    float x, y;
+	float x, y;
 } sVec2f;
 
 /* setting 0 vectors for the float and integer types */
@@ -32,225 +32,240 @@ const sVec2i iZERO = {0};
 const sVec2f fZERO = {0};
 
 /*
- * Initializes a new integer vector
- * @param   x
- *      vector x value
- * @param   y
- *      vector y value
- * @return  new integer vector
+ * @brief Initializes a new integer vector
+ *
+ * @param x
+ * 		Vector x value
+ * @param y
+ * 		Vector y value
+ * @return New integer vector
  */
 sVec2i sVec2iInit(int x, int y)
 {
-    sVec2i z = iZERO;
-    z.x = x;
-    z.y = y;
-    return z;
+	sVec2i z = iZERO;
+	z.x = x;
+	z.y = y;
+	return z;
 }
 
 /*
- * Initializes a new float vector
- * @param   x
- *      vector x value
- * @param   y
- *      vector y value
- * @return  new float vector
+ * @brief Initializes a new float vector
+ *
+ * @param x
+ * 		Vector x value
+ * @param y
+ * 		Vector y value
+ * @return New float vector
  */
 sVec2f sVec2fInit(float x, float y)
 {
-    sVec2f z = fZERO;
-    z.x = x;
-    z.y = y;
-    return z;
+	sVec2f z = fZERO;
+	z.x = x;
+	z.y = y;
+	return z;
 }
 
 /*
- * Adds two integer vectors together
- * @param   a
- *      first integer vector to add
- * @param   b
- *      second integer vector to add
- * @return  the integer vector addition of [a] and [b]
+ * @brief Adds two integer vectors together
+ *
+ * @param a
+ * 		First integer vector to add
+ * @param b
+ * 		Second integer vector to add
+ * @return The integer vector addition of [a] and [b]
  */
 sVec2i sVec2iAdd(sVec2i a, sVec2i b)
 {
-    sVec2i c = iZERO;
-    c.x = a.x + b.x;
-    c.y = a.y + b.y;
-    return c;
+	sVec2i c = iZERO;
+	c.x = a.x + b.x;
+	c.y = a.y + b.y;
+	return c;
 }
 
 /*
- * Adds two float vectors together
- * @param   a
- *      first float vector to add
- * @param   b
- *      second float vector to add
- * @return  the float vector addition of [a] and [b]
+ * @brief Adds two float vectors together
+ *
+ * @param a
+ * 		First float vector to add
+ * @param b
+ * 		Second float vector to add
+ * @return The float vector addition of [a] and [b]
  */
 sVec2f sVec2fAdd(sVec2f a, sVec2f b)
 {
-    sVec2f c = fZERO;
-    c.x = a.x + b.x;
-    c.y = a.y + b.y;
-    return c;
+	sVec2f c = fZERO;
+	c.x = a.x + b.x;
+	c.y = a.y + b.y;
+	return c;
 }
 
 /*
- * Subtracts two integer vectors from each other
- * @param   a
- *      first integer vector to subtract
- * @param   b
- *      second integer vector to subtract
- * @return  the integer vector subtraction of [a] and [b]
+ * @brief Subtracts two integer vectors from each other
+ *
+ * @param a
+ * 		First integer vector to subtract
+ * @param b
+ * 		Second integer vector to subtract
+ * @return The integer vector subtraction of [a] and [b]
  */
 sVec2i sVec2iSub(sVec2i a, sVec2i b)
 {
-    sVec2i c = iZERO;
-    c.x = a.x - b.x;
-    c.y = a.y - b.y;
-    return c;
+	sVec2i c = iZERO;
+	c.x = a.x - b.x;
+	c.y = a.y - b.y;
+	return c;
 }
 
 /*
- * Subtracts two float vectors from each other
- * @param   a
- *      first float vector to subtract
- * @param   b
- *      second float vector to subtract
- * @return  the float vector subtraction of [a] and [b]
+ * @brief Subtracts two float vectors from each other
+ *
+ * @param a
+ * 		First float vector to subtract
+ * @param b
+ * 		Second float vector to subtract
+ * @return The float vector subtraction of [a] and [b]
  */
 sVec2f sVec2fSub(sVec2f a, sVec2f b)
 {
-    sVec2f c = fZERO;
-    c.x = a.x - b.x;
-    c.y = a.y - b.y;
-    return c;
+	sVec2f c = fZERO;
+	c.x = a.x - b.x;
+	c.y = a.y - b.y;
+	return c;
 }
 
 /*
- * Checks if two integer vectors are equal
- * @param   a
- *      first integer vector to compare
- * @param   b
- *      second integer vector to compare
- * @return  [true] (if a and b are equal), [false] otherwise
+ * @brief Checks if two integer vectors are equal
+ *
+ * @param a
+ * 		First integer vector to compare
+ * @param b
+ * 		Second integer vector to compare
+ * @return [true] if a and b are equal, and [false] otherwise
  */
 boolean sVec2iEqual(sVec2i a, sVec2i b)
 {
-    return (a.x == b.x && a.y == b.y);
+	return (a.x == b.x && a.y == b.y);
 }
 
 /*
- * Checks if two float vectors are equal
- * @param   a
- *      first float vector to compare
- * @param   b
- *      second float vector to compare
- * @return  [true] (if a and b are equal), [false] otherwise
+ * @brief Checks if two float vectors are equal
+ *
+ * @param a
+ * 		First float vector to compare
+ * @param b
+ * 		Second float vector to compare
+ * @return [true] if a and b are equal, and [false] otherwise
  */
 boolean sVec2fEqual(sVec2f a, sVec2f b)
 {
-    return (a.x == b.x && a.y == b.y);
+	return (a.x == b.x && a.y == b.y);
 }
 
 /*
- * Computes the distance between two integer vectors
- * @param   a
- *      first integer vector
- * @param   b
- *      second integer vector
- * @return  integer value of distance between the two vectors
+ * @brief Computes the distance between two integer vectors
+ *
+ * @param a
+ * 		First integer vector
+ * @param b
+ * 		Second integer vector
+ * @return Integer value of distance between the two vectors
  */
 uint32_t sVec2iDistance(sVec2i a, sVec2i b)
 {
-    return (uint32_t) (distance(a.x, a.y, b.x, b.y));
+	return (uint32_t) (distance(a.x, a.y, b.x, b.y));
 }
 
 /*
- * Computes the distance between two float vectors
- * @param   a
- *      first float vector
- * @param   b
- *      second float vector
- * @return  float value of distance between the two vectors
+ * @brief Computes the distance between two float vectors
+ *
+ * @param a
+ * 		First float vector
+ * @param b
+ * 		Second float vector
+ * @return Float value of distance between the two vectors
  */
 float sVec2fDistance(sVec2f a, sVec2f b)
 {
-    return distance(a.x, a.y, b.x, b.y);
+	return distance(a.x, a.y, b.x, b.y);
 }
 
 /*
- * Computes the length of the integer vector
- * @param   a
- *      vector to measure length
- * @return  length of integer vector [a]
+ * @brief Computes the length of the integer vector
+ *
+ * @param a
+ * 		Vector to measure length
+ * @return Length of integer vector [a]
  */
 float sVec2iLength(sVec2i a)
 {
-    return ssqrt((a.x * a.x) + (a.y * a.y));
+	return ssqrt((a.x * a.x) + (a.y * a.y));
 }
 
 /*
- * Computes the length of the float vector
- * @param   a
- *      vector to measure length
- * @return  length of float vector [a]
+ * @brief Computes the length of the float vector
+ *
+ * @param a
+ * 		Vector to measure length
+ * @return Length of float vector [a]
  */
 float sVec2fLength(sVec2f a)
 {
-    return ssqrt((a.x * a.x) + (a.y * a.y));
+	return ssqrt((a.x * a.x) + (a.y * a.y));
 }
 
 /*
- * Normalize integer vector
- * @param   a
- *      integer vector to normalize
- * @return  normalized vector
+ * @brief Normalize integer vector
+ *
+ * @param a
+ * 		Integer vector to normalize
+ * @return Normalized vector
  */
 sVec2i sVec2iNormalize(sVec2i a)
 {
-    sVec2i b = iZERO;
-    float len = sVec2iLength(a);
-    b.x = a.x / len;
-    b.y = a.y / len;
-    return b;
+	sVec2i b = iZERO;
+	float len = sVec2iLength(a);
+	b.x = a.x / len;
+	b.y = a.y / len;
+	return b;
 }
 
 /*
- * Normalize float vector
- * @param   a
- *      float vector to normalize
- * @return  normalized vector
+ * @brief Normalize float vector
+ *
+ * @param a
+ * 		Float vector to normalize
+ * @return Normalized vector
  */
 sVec2f sVec2fNormalize(sVec2f a)
 {
-    sVec2f b = fZERO;
-    float len = sVec2fLength(a);
-    b.x = a.x / len;
-    b.y = a.y / len;
-    return b;
+	sVec2f b = fZERO;
+	float len = sVec2fLength(a);
+	b.x = a.x / len;
+	b.y = a.y / len;
+	return b;
 }
 
 /*
- * Computes the angle the integer vector is facing in radians
- * @param   a
- *      vector to compute angle of
- * @return  angle the vector is facing in radians
+ * @brief Computes the angle the integer vector is facing in radians
+ *
+ * @param a
+ * 		Vector to compute angle of
+ * @return Angle the vector is facing in radians
  */
 float sVec2iAngle(sVec2i a)
 {
-    return sarctan((float)(a.y) / (float)(a.x));
+	return sarctan((float)(a.y) / (float)(a.x));
 }
 
 /*
  * Computes the angle the float vector is facing in radians
- * @param   a
- *      vector to compute angle of
- * @return  angle the vector is facing in radians
+ * @param a
+ * 		Vector to compute angle of
+ * @return Angle the vector is facing in radians
  */
 float sVec2fAngle(sVec2f a)
 {
-    return sarctan(a.y / a.x);
+	return sarctan(a.y / a.x);
 }
 
 #ifdef __cplusplus
